ee0ad3ed447ab8859edbceeb34cf11771f185859
StevenBlythe <83834901+StevenBlythe@users.noreply.github.com>
2021-11-25 00:41:33 -0500

    Update

--- Manim/SIR.py
+++ Manim/SIR.py
@@ -22,7 +22,27 @@ Colors = [
     "#E2716A",
     "#EB6C63",
     "#F3675C",
-    "#FC6255"
+    "#FC6255",
+    "#F8645B",
+    "#F56660",
+    "#F16866",
+    "#ED6A6C",
+    "#E96C71",
+    "#E66E77",
+    "#E2707D",
+    "#DE7282",
+    "#DA7488",
+    "#D7768E",
+    "#D37793",
+    "#CF7999",
+    "#CB7B9E",
+    "#C87DA4",
+    "#C47FAA",
+    "#C081AF",
+    "#BC83B5",
+    "#B985BB",
+    "#B587C0",
+    "#B189C6"
 ]
 
 Colors_ten = [
@@ -74,10 +94,11 @@ class ExponentialGrowth(Scene):
                 graphs = add_exponential_graph(self, ax, Colors[i], i/2, k)
                 self.play(Write(graphs), run_time=1)
 
-class ExponentialGrowth(Scene):
+class ExponentialDecay(Scene):
     def construct(self):
-        def add_exponential_graph(self, axes, color, c, k):
-            f = lambda t : c*math.e**(k*t)
+        def add_exponential_graph(self, axes, color, c, k, N):
+            b = (10/c) - 1
+            f = lambda t : N/(1 + b*math.e**(-k*t))
             graph = axes.plot(
                 f, x_range=[0, 20], color = color
             )
@@ -96,24 +117,96 @@ class ExponentialGrowth(Scene):
 
 
         # Our exponential parameters
-        c = 1 # Initial Condition
-        k = 0.15 # Rate of Growth
+        N = 10 # Limiting Capacity
+        c = 1 # Desired initial population
+        k = 0.25 # Rate of Growth
 
         # Create one graph and add it to Axes:
-        self.play(Write(add_exponential_graph(self, ax, "#69BACF", c, k)))
+        self.play(Write(add_exponential_graph(self, ax, Colors[c], c, k, N)))
 
         # Add a point and track the value.
 
         # Create other solutions
         # Colors for the lines
-
-        for i in range(0, 20, 1):
-            if i != 2:
-                graphs = add_exponential_graph(self, ax, Colors[i], i/2, k)
+        for i in range(1, 40, 1):
+            if i != c*2:
+                graphs = add_exponential_graph(self, ax, Colors[i], i/2, k, N)
                 self.play(Write(graphs), run_time=1)
+
+class PredatorPreyModelWrong(MovingCameraScene):
+    def construct(self):
+
+        # Standard axes
+        ax = Axes(
+            tips=False
+        )
+        self.play(Write(ax))
+
+        # Vector Field
+        colors = [RED, YELLOW, BLUE, DARK_GRAY]
+        f = lambda pos : (2 * pos[0] - 1.6 * pos[0] * pos[1])* RIGHT + (-1 * pos[1] + 0.8 * pos[1] * pos[0]) * UP
+        vector_field = ArrowVectorField(
+            f,
+            colors=Colors
+            )
+        self.play(Write(vector_field))
+        self.wait(1)
+        self.play(Uncreate(ax), Uncreate(vector_field))
+
+
+class PredatorPreyModelGraph(Scene):
+    def construct(self):
+        f = lambda pos : (2 * pos[0] - 1.6 * pos[0] * pos[1])* RIGHT + (-1 * pos[1] + 0.8 * pos[1] * pos[0]) * UP
+        
+        vector_field = ArrowVectorField(
+            f,
+            colors=Colors
+            )
+        
+        ax = Axes(
+            x_range = [0, 10, 1],
+            y_range = [0, 10, 1],
+            tips=False
+        )
+
+        vector_field = ArrowVectorField(
+            f,
+            x_range = [0, 11.5, 0.5],
+            y_range = [0, 6, 0.5]
+        )
+        dot = Dot(point = ax.coords_to_point(0.0195, 0.036))
+
+        vector_field.align_to(dot, DL)
+        self.play(Write(ax), Write(vector_field))
+
+        stream_lines = StreamLines(
+            f,
+            x_range = [0.1, 15, 0.5],
+            y_range = [0.15, 10, 0.5],
+            virtual_time = 1.5,
+            opacity = 1,
+            stroke_width=0.7,
+            color = PURPLE
+        )
+        stream_lines.align_to(dot, DL)
+        stream_lines.shift(UP*0.05)
+        self.add(stream_lines)
+        stream_lines.start_animation(warm_up=True, flow_speed=1)
+        self.wait(10)
+        self.play(Uncreate(stream_lines))
+
+class PredatorPreyModelRF(Scene):
+    def construct(self):
+        ax = Axes(
+            x_range = [0, 10, 1],
+            y_range = [0, 10, 1],
+            tips = False
+        )
+        print('Nothing yet')
 
 
 if __name__ == "__main__":
     os.system('cls')
-    os.system('manim ".\SIR.py" ExponentialGrowth -sp')
-    #os.system('manim ".\SIR.py" ExponentialGrowth -q k -p')
+    os.system('manim ".\SIR.py" PredatorPreyModel -p')
+    #os.system('manim ".\SIR.py" PredatorPreyModel -sp')
+    #os.system('manim ".\SIR.py" PredatorPreyModel -q k -p')
